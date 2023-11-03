faa7c63b3be0a1eb0423e5b85b83e5b193db56b3
jabiraziz <jabiraziz430@gmail.com>
2023-11-04 03:54:19 +0500

    added few more queries

--- customer/views.py
+++ customer/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.db.models import Count, Sum, Q
 
 from .models import Customers
+from purchase.models import Purchase
 
 
 # Create your views here.
@@ -23,6 +24,10 @@ def query_customers(request):
 
     # order by age, - represent descending order
     customer_ordering = Customers.objects.order_by("-age")
-    print(customer_ordering)
+
+    """purchases - is the relative_name of Purchase model, it let Customer(parent) model to access
+    the fields"""
+    add_purchase_amount = Customers.objects.aggregate(Sum("purchases__amount"))
+    print(add_purchase_amount)
 
     return HttpResponse("main")

caaa09f691c09c774d053636916573fadd936f34
jabiraziz <jabiraziz430@gmail.com>
2023-11-03 22:20:07 +0500

    initial commit

--- customer/views.py
+++ customer/views.py
@@ -3,15 +3,24 @@ from django.http import HttpResponse
 from django.db.models import Count, Sum, Q
 
 from .models import Customers
+
+
 # Create your views here.
 def query_customers(request):
+    # get all customers
     all_customers = Customers.objects.all()
-    customers_age = Customers.objects.values('age').aggregate(Count('age'))
-    customers_age = Customers.objects.values('age').aggregate(Sum('age'))
+    # count how many customers have added their age
+    """aggregate function basically accept multiple query , perform the 
+    intended operation on it and return single value."""
+    customers_age = Customers.objects.values("age").aggregate(Count("age"))
+    # add the ages of all the customers and return me sum of it
+    customers_age = Customers.objects.values("age").aggregate(Sum("age"))
 
-    cust_name = Customers.objects.filter(Q(name__startswith='I') | Q(name__startswith='N'))
+    """For or, and and not we use Q objects in django."""
+    cust_name = Customers.objects.filter(
+        Q(name__startswith="I") | Q(name__startswith="N")
+    )
 
     print(cust_name)
 
-    return HttpResponse('main')
-
+    return HttpResponse("main")

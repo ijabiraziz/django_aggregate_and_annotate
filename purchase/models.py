80712d002f9edd717d8d891ee90b40a1e1dc795b
jabiraziz <jabiraziz430@gmail.com>
2023-11-04 03:53:01 +0500

    created purchase app

--- purchase/models.py
+++ purchase/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from customer.models import Customers
 
 # Create your models here.
+class Purchase(models.Model):
+    amount = models.FloatField(help_text="in PKR")
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE, related_name='purchases')
+
+    def __str__(self) -> str:
+        return self.customer.name
+    
+    class Meta:
+        verbose_name_plural = 'Purchase'
